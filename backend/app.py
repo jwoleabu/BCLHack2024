0fee96b6535634930a31ba3ef3bad4cf6b2793fe
Moeed-Chughtai <95437123+Moeed-Chughtai@users.noreply.github.com>
2024-06-15 14:49:32 +0100

    testing backend get request

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,11 @@
 from flask import Flask
+import requests as re
 import sqlite3 
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    req = re.get('https://api.electoralcommission.org.uk/api/v1/postcode/SW1A1AA')
+    print(req.json())
+    return 'Hello, World!'
