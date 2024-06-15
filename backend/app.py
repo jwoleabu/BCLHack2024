5785ba9db2b5800d13b363451e95a1629cabe930
Moeed-Chughtai <95437123+Moeed-Chughtai@users.noreply.github.com>
2024-06-15 18:34:17 +0100

    combined enpoints

--- backend/app.py
+++ backend/app.py
@@ -15,9 +15,32 @@ def home():
     return "Hello World"
 
 
-## Returns the registration information for a given postcode
 @app.route('/postcode/<postcode>', methods=['GET'])
-@cross_origin(origin="http://localhost:3000/")
+def postcode(postcode):
+    url = f'https://developers.democracyclub.org.uk/api/v1/postcode/{postcode}?auth_token={os.getenv("DEMOCRACY_CLUB_TOKEN")}'
+    req = re.get(url)
+    data = req.json()
+    registration = data.get('registration', {})
+
+    candidates = data['dates'][0]['ballots'][0]['candidates']
+    candidates_info = []
+    for candidate in candidates:
+        candidate_info = {
+            "name": candidate["person"]["name"],
+            "party": candidate["party"]["party_name"],
+            "email": candidate["person"]["email"],
+            "absolute_url": candidate["person"]["absolute_url"],
+            "photo_url": candidate["person"]["photo_url"]
+        }
+        candidates_info.append(candidate_info)
+
+    response = {'registration': registration, 'candidates': candidates_info}
+    return jsonify(response)
+    
+
+
+## Returns the registration information for a given postcode
+""" @app.route('/postcode/<postcode>', methods=['GET'])
 def postcode(postcode):
     headers = {
         'Authorization': f'Bearer {os.getenv("ELECTORAL_COMMISSION_TOKEN")}'
@@ -27,7 +50,7 @@ def postcode(postcode):
     data = req.json()
     registration = data.get('registration', {})
     response = {'registration': registration}
-    return jsonify(response)
+    return jsonify(response) """
 
 
 ## Returns the candidates for a given postcode with their information
