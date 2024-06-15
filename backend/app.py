596ed54d6786a69b5d33e68deed1a9952fe2ea33
Pythe <jwoleabu@gmail.com>
2024-06-15 13:45:34 +0100

    initial endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,12 @@
 from flask import Flask
-import sqlite3 
+import sqlite3
+from flask_cors import cross_origin
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+@cross_origin("http://localhost:3000/")
+def home():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
